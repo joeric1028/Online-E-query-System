b8db07e70246f051205f79191606ab51ddc0d629
Jose Mari Ricardo <joeric1028@icloud.com>
2019-01-31 09:50:18 +0800

    Added Models to autoload feature
    Removing the model initializer from controller for giving less codes as the models were used often.

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -4,8 +4,6 @@ class News extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('news_model');
-        $this->load->helper('url_helper');
     }
 
     public function index()
