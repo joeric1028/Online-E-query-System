316d5f70b0291acd927d4d864a2f5be5fea9430b
Jose Mari Ricardo <joeric1028@icloud.com>
2019-02-04 17:42:33 +0800

    Permamently Renamed Filename

--- application/controllers/Schoolcalendar.php
+++ application/controllers/Schoolcalendar.php
@@ -1,11 +1,6 @@
 <?php
 class SchoolCalendar extends CI_Controller {
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
     public function index()
     {
         $data['title'] = 'Welcome to E-Query System!';
