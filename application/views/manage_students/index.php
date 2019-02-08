56d2245a53a455bb3e13659808045a539ee1ba12
Kyle Shem Montealto <kylegmontealto@su.edu.ph>
2019-02-09 02:29:16 +0800

    Added Modals to Manage Students

--- application/views/manage_students/index.php
+++ application/views/manage_students/index.php
@@ -26,7 +26,7 @@
         <div class="card" id="subjectCard">
             <div class="card-header d-flex justify-content-between">
                 <span>Subjects</span>
-                <button class="btn btn-outline-success btn-sm" disabled type="button" id="addSubjectBtn">Add Subject</button>    
+                <button class="btn btn-outline-success btn-sm" type="button" id="addSubjectBtn" data-toggle="modal" data-target="#addSubjectModal">Add Subject</button>    
             </div>
             <div class="card-body">
                 <ul class="list-group">
@@ -56,7 +56,7 @@
         <div class="card" id="studentCard">
             <div class="card-header d-flex justify-content-between">
                 <span>Students</span>
-                <button class="btn btn-outline-success btn-sm" type="button" id="addStudentBtn">Add Student</button>    
+                <button class="btn btn-outline-success btn-sm" type="button" id="addStudentBtn" data-toggle="modal" data-target="#addStudentModal">Add Student</button>    
             </div>
             
             <div class="card-body">
@@ -79,8 +79,110 @@
     </div>
 </div>
 
+<div id="addSubjectModal" class="modal fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+    <form name="addsubjectform" id="addsubjectform" action="" method="post">
+      <div class="modal-header">
+        <h5 class="modal-title">Add Subject</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close" id="addsubjectclosex">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-group">
+          <label class="form-label">Subject Name</label>
+          <input type="text" class="form-control" name="subjectname" id="subjectname">
+          <label class="error text-danger" for="subjectname" id="subjectname_error">This field is required.</label>
+        </div>
+      </div>
+      <div class="modal-footer">
+	  <label class="error form-label text-success" id="statussuccess"></label>
+      <label class="error form-label text-danger" id="statuserror"></label>
+      <button type="submit" class="btn btn-primary" id="addevent">Add</button>
+      <button type="button" class="btn btn-secondary" data-dismiss="modal" id="addsubjectclose">Cancel</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<div id="addStudentModal" class="modal fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+    <form name="addStudentform" id="addStudentform" action="" method="post">
+      <div class="modal-header">
+        <h5 class="modal-title">Add Student</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-group">
+          <label class="form-label">First Name</label>
+          <input type="text" class="form-control" name="firstname" id="firstname">
+          <label class="error text-danger" for="firstname" id="firstname_error">This field is required.</label>
+        </div>
+        <div class="form-group">
+          <label class="form-label">Middle Name</label>
+          <input type="text" class="form-control" name="middlename" id="middlename">
+          <label class="error text-danger" for="middlename" id="middlename_error">This field is required.</label>
+        </div>
+        <div class="form-group">
+          <label class="form-label">Last Name</label>
+          <input type="text" class="form-control" name="lastname" id="lastname">
+          <label class="error text-danger" for="lastname" id="lastname_error">This field is required.</label>
+        </div>
+        <div class="form-group">
+          <label class="form-label">ID Number</label>
+          <input type="text" class="form-control" name="idnumber" id="idnumber">
+          <label class="error text-danger" for="idnumber" id="idnumber_error">This field is required.</label>
+        </div>
+        <div class="form-group">
+          <div class="form-label">Gender</div>
+          <div class="custom-controls-stacked">
+            <label class="custom-control custom-radio custom-control-inline">
+              <input type="radio" class="custom-control-input" name="gender" id="gender" value="Male" checked="">
+              <span class="custom-control-label">Male</span>
+            </label>
+            <label class="custom-control custom-radio custom-control-inline">
+              <input type="radio" class="custom-control-input" name="gender" id="gender" value="Female">
+              <span class="custom-control-label">Female</span>
+            </label>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="form-label">Grade Level</label>
+            <select class="form-control custom-select" name="type" id="type">
+                <option value="1">Grade 1</option>
+                <option value="2">Grade 2</option>
+                <option value="3">Grade 3</option>
+                <option value="4">Grade 4</option>
+                <option value="5">Grade 5</option>
+                <option value="6">Grade 6</option>
+            </select>
+        </div>
+      </div>
+      <div class="modal-footer">
+	  <label class="error form-label text-success" id="statussuccess"></label>
+	  <label class="error form-label text-danger" id="statuserror"></label>
+		<button type="submit" class="btn btn-primary" id="addUser">Add</button>
+		<button type="button" class="error btn btn-primary" id="addAnotherUserAdd">Add Another User?</button>
+		<button type="button" class="error btn btn-secondary" data-dismiss="modal" id="addAnotherUserClose">Close</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" id="addUserCancel">Cancel</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
 
 <script>
+    $('.error').hide();
+    $('#addSubjectModal').on('shown.bs.modal', function () {
+        $('#subjectname').trigger('focus')
+    })
+
     $('.dropdown-menu a').click(function(){
         if($(this).text() == "All Levels") {
             $('#addSubjectBtn').attr('disabled',true);
